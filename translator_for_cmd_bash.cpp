88cdc6ec14be31c5b1a68c83ce49f83a02c57ae9
Anthonyzok521 <anthonyzok521@gmail.com>
2025-02-09 21:34:40 -0400

    initial commit

---
+++ translator_for_cmd_bash.cpp
@@ -0,0 +1,12 @@
+﻿// translator_for_cmd_bash.cpp : Defines the entry point for the application.
+//
+
+#include "translator_for_cmd_bash.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
